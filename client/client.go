c552fe80b6aa5a4699a2a0f0569ecfe69ae75abf
Eugene Shevekhman <yevshev@users.noreply.github.com>
2019-10-26 13:19:56 -0500

    initial commit

---
+++ client/client.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type CPUTempObj struct {
+	TimeStamp   time.Time
+	HostAddress string
+	CPUTemp     float64
+}
+
+func lambdaStateDiscovery(v CPUTempObj) (string, float64, string, string) {
+
+	cpu_temp := v.CPUTemp
+	cpu_temp_state := "CPU_TEMP_NONDETERMINISTIC"
+	host_address := v.HostAddress
+	timestamp := v.TimeStamp.Format(time.StampNano)
+
+	if cpu_temp <= 3 || cpu_temp >= 98 {
+		cpu_temp_state = "CPU_TEMP_CRITICAL"
+	} else if cpu_temp >= 93 && cpu_temp < 98 {
+		cpu_temp_state = "CPU_TEMP_HIGH"
+	} else if cpu_temp > 3 && cpu_temp < 93 {
+		cpu_temp_state = "CPU_TEMP_OK"
+	}
+	return timestamp, cpu_temp, cpu_temp_state, host_address
+
+}
+
+func collectCPUTemperature(hostName string) {
+
+	resp, err := http.Get("http://" + hostName + "/redfish/v1/Chassis/1/Thermal")
+	if err != nil {
+		return
+	}
+
+	var result CPUTempObj
+	byteResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.Unmarshal(byteResp, &result)
+	timestamp, cpu_temp, cpu_temp_state, host_address := lambdaStateDiscovery(result)
+	fmt.Printf("%v %s %.2fC %s\n", timestamp, host_address, cpu_temp, cpu_temp_state)
+}
+func main() {
+
+	var nodeList [20]string
+
+	//Fill array with server hostnames
+	for i := range nodeList {
+		//var nodeNum = strconv.Itoa(i)
+		nodeList[i] = "localhost" /*+ nodeNum*/ + ":8000"
+	}
+
+	var wg sync.WaitGroup
+
+	for _, node := range nodeList {
+		wg.Add(1)
+		go func(nodeAddress string) {
+			defer wg.Done()
+			collectCPUTemperature(nodeAddress)
+		}(node)
+	}
+	wg.Wait()
+	//collectCPUTemperature("localhost:8000")
+}
---
+++ server/server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
+
+type CPUTempObj struct {
+	TimeStamp   time.Time
+	HostAddress string
+	CPUTemp     float64
+}
+
+func ResponseServer(w http.ResponseWriter, r *http.Request) {
+	w.Write(GetCPUTemp())
+}
+
+func randTemperature(min, max float64) float64 {
+	rand.Seed(time.Now().UnixNano())
+	return math.Floor((min+rand.Float64()*(max-min))*100) / 100
+}
+
+// CPU temperature
+func GetCPUTemp() []byte {
+	hostName, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	//log.Println("CPU temperature")
+
+	//Its a mockup CPU temperature
+	cpuTempObj := new(CPUTempObj)
+	cpuTempObj.TimeStamp = time.Now()
+	cpuTempObj.HostAddress = hostName
+	cpuTempObj.CPUTemp = randTemperature(3.0, 98.0)
+
+	jsonObj, err := json.Marshal(cpuTempObj)
+	if err != nil {
+		log.Println(fmt.Sprintf("Could not marshal the response data: %v", err))
+	}
+	return jsonObj
+
+}
+
+func main() {
+
+	// Register with /events endpoint.
+	http.HandleFunc("/redfish/v1/", ResponseServer)
+
+	// Get hostname
+	//hostName, err := os.Hostname()
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//http.ListenAndServe(hostName+":8000", nil)
+	http.ListenAndServe(":8000", nil)
+}
